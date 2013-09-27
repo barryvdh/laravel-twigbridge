ffbfc094fa68bef017ab0f78a6b15ea63b3f18d7
Barry vd. Heuvel <barry@fruitcakestudio.nl>
2013-09-27 10:07:50 +0200

    Small cleanup

--- src/Barryvdh/TwigBridge/TwigTemplate.php
+++ src/Barryvdh/TwigBridge/TwigTemplate.php
@@ -3,7 +3,7 @@
 namespace Barryvdh\TwigBridge;
 
 use Twig_Template;
-
+use Illuminate\View\View;
 
 /**
  * Default base class for compiled templates.
@@ -11,27 +11,20 @@ use Twig_Template;
 abstract class TwigTemplate extends Twig_Template
 {
 
-    protected function getViewName(){
-        $name = $this->getTemplateName();
-
-        return $name;
-
-
-    }
+    /**
+     * {@inheritdoc}
+     */
     public function display( array $context, array $blocks = array())
     {
-
+        $name = $this->getTemplateName();
         //The first time, PathLoader is used so we only have the full path, not a name. The first creator/composers are run from View::make, so just skip that.
-        if(!is_file($this->getTemplateName())){
-
+        if(!is_file($name)){
+            /** @var \Illuminate\View\Environment $env */
             $env  = $context['__env'];
-            \View::callCreator($view = new \Illuminate\View\View($env, $env->getEngineResolver()->resolve('twig'), $this->getViewName(), null, $context));
-
+            \View::callCreator($view = new View($env, $env->getEngineResolver()->resolve('twig'), $name, null, $context));
             \View::callComposer($view);
-
             $context = $view->getData();
         }
-
         try{
             parent::display($context, $blocks);
         }catch(\Exception $e){
@@ -41,23 +34,17 @@ abstract class TwigTemplate extends Twig_Template
         }
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function getAttribute($object, $item, array $arguments = array(), $type = Twig_Template::ANY_CALL, $isDefinedTest = false, $ignoreStrictCheck = false){
-
         if(
             Twig_Template::METHOD_CALL !== $type //Don't handle Method Calls
             and is_a($object,'Illuminate\Database\Eloquent\Model') //Only handle Models
-         /*   and (
-                isset($object->{$item})     //Normal attribute
-                or $object->hasGetMutator($item)    //via Mutator
-                or method_exists($object, camel_case($item))    //Relation
-            )*/
         ){
             return $object->getAttribute($item);
         }else{
             return parent::getAttribute($object, $item, $arguments, $type, $isDefinedTest, $ignoreStrictCheck);
         }
-
     }
-
-
 }
