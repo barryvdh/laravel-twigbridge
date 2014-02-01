0632d691040929cffc8f373ee430f19c3a21a05c
Barry vd. Heuvel <barry@fruitcakestudio.nl>
2014-02-01 15:18:32 +0100

    Improve config file

--- src/config/config.php
+++ src/config/config.php
@@ -2,20 +2,44 @@
 
 return array(
 
-
  /*
   |--------------------------------------------------------------------------
   | Twig options
   |--------------------------------------------------------------------------
   |
-  | Standard Twig settings; http://twig.sensiolabs.org/doc/api.html#environment-options
+  | Available options:
+  |
+  |   * debug: When set to true, it automatically set "auto_reload" to true as
+  |           well (default to app.debug setting).
+  |
+  |   * charset: The charset used by the templates (default to UTF-8).
+  |
+  |   * base_template_class: The base template class to use for generated
+  |                         templates (default to Barryvdh\TwigBridge\TwigTemplate).
+  |
+  |   * cache: An absolute path where to store the compiled templates, or
+  |           false to disable compilation cache. Default is $app['path.storage'].'/views/twig'
+  |
+  |   * auto_reload: Whether to reload the template if the original source changed.
+  |                 If you don't provide the auto_reload option, it will be
+  |                 determined automatically based on the debug value.
+  |
+  |   * strict_variables: Whether to ignore invalid variables in templates
+  |                      (default to false).
+  |
+  |   * autoescape: Whether to enable auto-escaping (default to html):
+  |                   * false: disable auto-escaping
+  |                   * true: equivalent to html
+  |                   * html, js: set the autoescaping to one of the supported strategies
+  |                   * PHP callback: a PHP callback that returns an escaping strategy based on the template "filename"
+  |
+  |   * optimizations: A flag that indicates which optimizations to apply
+  |                   (default to -1 which means that all optimizations are enabled;
+  |                   set it to 0 to disable).
   |
   */
     'options' => array(
-        'debug' => Config::get('app.debug'),
-        'strict_variables' => Config::get('app.debug'),
-        'auto_reload' =>  Config::get('app.debug'),
-        'autoescape' => true,
+
     ),
 
 
@@ -25,19 +49,19 @@ return array(
    |--------------------------------------------------------------------------
    |
    | List of Functions & Filters that are made available to your Twig templates.
-   | Supports string or closure.
+   | Supports string, array, closure or Twig_SimpleFilter / Twig_SimpleFunction .
    |
    */
     'functions' => array(
-       
+
     ),
 
     'filters' => array(
-      
+
     ),
 
     'facades' => array(
-      
+
     )
 
 
