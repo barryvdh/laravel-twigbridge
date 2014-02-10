9e501d2ecfcc7ad7a36e74ed82c5e7e078f56cc1
Barry vd. Heuvel <barryvdh@gmail.com>
2014-02-10 19:16:48 +0100

    Simplify TwigEngine
    The TwigEngine only loads templates based on a path. The path is already checked by the ViewFinder, is always a string so never a Twig_TemplateInterface instance.

--- src/Barryvdh/TwigBridge/TwigEngine.php
+++ src/Barryvdh/TwigBridge/TwigEngine.php
@@ -14,7 +14,7 @@ class TwigEngine implements EngineInterface
     /**
      * Constructor.
      *
-     * @param \Twig_Environment $environment A \Twig_Environment instance
+     * @param \Twig_Environment $environment A Twig_Environment instance
      */
     public function __construct(\Twig_Environment $environment)
     {
@@ -27,10 +27,11 @@ class TwigEngine implements EngineInterface
      * It also supports \Twig_Template as name parameter.
      *
      * @throws \Twig_Error if something went wrong like a thrown exception while rendering the template
+     * @throws \Twig_Error_Loader if the template cannot be loaded
      */
     public function get($path, array $data = array())
     {
-        $template = $this->load($path);
+        $template = $this->environment->loadTemplate($path);
         
         if($template instanceof TwigTemplate){
             //Events are already fired by the View Environment
@@ -38,28 +39,6 @@ class TwigEngine implements EngineInterface
         }
         
         return $template->render($data);
-    }
-
-    /**
-     * Loads the given template.
-     *
-     * @param string|TemplateReferenceInterface|\Twig_Template $name A template name or an instance of Twig_Template
-     *
-     * @return \Twig_TemplateInterface A \Twig_TemplateInterface instance
-     *
-     * @throws \InvalidArgumentException if the template does not exist
-     */
-    protected function load($name)
-    {
-        if ($name instanceof \Twig_Template) {
-            return $name;
-        }
-
-        try {
-            return $this->environment->loadTemplate((string) $name);
-        } catch (\Twig_Error_Loader $e) {
-            throw new \InvalidArgumentException($e->getMessage(), $e->getCode(), $e);
-        }
     }
 
 }
